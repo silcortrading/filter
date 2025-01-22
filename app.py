f26175403ad6ee8e7d11d29db658852d331e3d74
silcortrading <ti@silcor.com.br>
2025-01-22 09:50:04 -0300

    Corrigir binding para 0.0.0.0

--- app.py
+++ app.py
@@ -198,4 +198,4 @@ def download_file(folder, filename):
     return send_from_directory(f'./{folder}', filename, as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=False)  
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))  
